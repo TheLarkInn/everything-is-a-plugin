4ddd79d68cfe54b258b1ad9bff31415bc139c2d6
Sean Larkin <sean.larkin@cuw.edu>
2017-03-25 09:00:10 -0500

    1.2

--- plugin-lessons-plugins/1BasicPluginWebpackPlugin.js
+++ plugin-lessons-plugins/1BasicPluginWebpackPlugin.js
@@ -16,18 +16,29 @@ class BasicPlugin {
          * Part 2
          * How to hook.
          */
-        compiler.plugin("run", (compiler, callback) => {
+        // compiler.plugin("run", (compiler, callback) => {
+        //     const pinkMessage = chalk.magenta("\n\nHello World\n\n");
+        //     console.log(pinkMessage);
+
+        //     callback();
+        // });
+        // compiler.plugin("watch-run", (compiler, callback) => {
+        //     const pinkMessage = chalk.magenta("\n\nHello World\n\n");
+        //     console.log(pinkMessage);
+
+        //     callback();
+        // }); 
+        /**
+         * Part 3
+         * How to hook (multi-hook)
+         */
+
+        compiler.plugin(["run","watch-run"], (compiler, callback) => {
             const pinkMessage = chalk.magenta("\n\nHello World\n\n");
             console.log(pinkMessage);
 
             callback();
         });
-        compiler.plugin("watch-run", (compiler, callback) => {
-            const pinkMessage = chalk.magenta("\n\nHello World\n\n");
-            console.log(pinkMessage);
-
-            callback();
-        }); 
     }
 }
 
