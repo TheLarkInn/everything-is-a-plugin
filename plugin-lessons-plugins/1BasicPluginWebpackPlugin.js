f88adab132ee160627f3777ad49a623bcc08ff35
Sean Larkin <sean.larkin@cuw.edu>
2017-03-25 09:02:27 -0500

    1.3

--- plugin-lessons-plugins/1BasicPluginWebpackPlugin.js
+++ plugin-lessons-plugins/1BasicPluginWebpackPlugin.js
@@ -3,36 +3,9 @@ const pluginUtils = require("./plugin-utils");
 
 class BasicPlugin {
     constructor() {
-        /**
-         * Part 1
-         * Constructors on webpack plugins are optional. 
-         * They are only needed if you are planning on allowing your user to pass options.
-         * 
-         */
          pluginUtils.logPluginEvent("pluginDidMount", "BasicPlugin");
     }
     apply(compiler) {
-        /**
-         * Part 2
-         * How to hook.
-         */
-        // compiler.plugin("run", (compiler, callback) => {
-        //     const pinkMessage = chalk.magenta("\n\nHello World\n\n");
-        //     console.log(pinkMessage);
-
-        //     callback();
-        // });
-        // compiler.plugin("watch-run", (compiler, callback) => {
-        //     const pinkMessage = chalk.magenta("\n\nHello World\n\n");
-        //     console.log(pinkMessage);
-
-        //     callback();
-        // }); 
-        /**
-         * Part 3
-         * How to hook (multi-hook)
-         */
-
         compiler.plugin(["run","watch-run"], (compiler, callback) => {
             const pinkMessage = chalk.magenta("\n\nHello World\n\n");
             console.log(pinkMessage);
