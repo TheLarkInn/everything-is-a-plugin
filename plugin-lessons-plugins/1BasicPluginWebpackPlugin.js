06d80d17d5a10da7684c57720e72922642a094a3
Sean Larkin <sean.larkin@cuw.edu>
2017-03-25 09:07:47 -0500

    1.quiz

--- plugin-lessons-plugins/1BasicPluginWebpackPlugin.js
+++ plugin-lessons-plugins/1BasicPluginWebpackPlugin.js
@@ -3,12 +3,23 @@ const pluginUtils = require("./plugin-utils");
 
 class BasicPlugin {
     constructor() {
-         pluginUtils.logPluginEvent("pluginDidMount", "BasicPlugin");
+        pluginUtils.logPluginEvent("pluginDidMount", "BasicPlugin");
+        
+        /**
+         * MINI QUIZ!!! MINI QUIZ!!! MINI QUIZ!!! MINI QUIZ!!! MINI QUIZ!!! MINI QUIZ!!! MINI QUIZ!!! MINI QUIZ!!!
+         * 
+         * @description - Pass an option "message" into your plugin. Use that message and console.log it when webpack / webpack-dev-server 
+         *              - "run"'s or "watch-run"'s
+         * 
+         * PROTIP: Don't forget to actually pass the message in the plugin in your webpack config!
+         * 
+         * 
+         * MINI QUIZ!!! MINI QUIZ!!! MINI QUIZ!!! MINI QUIZ!!! MINI QUIZ!!! MINI QUIZ!!! MINI QUIZ!!! MINI QUIZ!!!
+         */ 
     }
+
     apply(compiler) {
         compiler.plugin(["run","watch-run"], (compiler, callback) => {
-            const pinkMessage = chalk.magenta("\n\nHello World\n\n");
-            console.log(pinkMessage);
 
             callback();
         });
