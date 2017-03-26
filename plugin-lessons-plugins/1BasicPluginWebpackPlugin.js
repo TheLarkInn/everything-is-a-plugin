b8e1293989d88a975d079c719101c77ec4f12ab2
Sean Larkin <sean.larkin@cuw.edu>
2017-03-26 14:15:48 -0500

    1.6

--- plugin-lessons-plugins/1BasicPluginWebpackPlugin.js
+++ plugin-lessons-plugins/1BasicPluginWebpackPlugin.js
@@ -1,14 +1,27 @@
 const chalk = require("chalk");
 const pluginUtils = require("./plugin-utils");
 
-function BasicPlugin(message) {
-    this.message = message;
-}
-
-BasicPlugin.prototype.apply = function apply(compiler) {
-    compiler.plugin("run", (compiler, callback) => {
-        pluginUtils.logPluginEvent(`compiler.run.message.${this.message}`, "BasicPlugin");
-    });
+/**
+ * @class BasicPlugin - This is a example of a webpack plugin using `class`. The most common and easy to understand syntax
+ * 
+ */
+class BasicPlugin {
+    /**
+     * @description Creates an instance of BasicPlugin.
+     * @param {object} options - Adds options from your configuration into the plugin for additional functionality
+     * 
+     * @memberOf BasicPlugin
+     */
+    constructor(options) {}
+    /**
+     * @name apply
+     * @description This method is invoked by the Compiler on "registration" and an instance of the Compiler is passed in as a parameter. 
+     * @param {Compiler} compiler - This is the Compiler instance. Calling `compiler.plugin('some-event', (someState) => {})` allows one to "plug into" 
+     * the compiler and listen to events and update state/add side effects or perform additional functionality through the compiler/compilation life cycle
+     * 
+     * @memberOf BasicPlugin
+     */
+    apply(compiler) {}
 }
 
 module.exports = BasicPlugin;
